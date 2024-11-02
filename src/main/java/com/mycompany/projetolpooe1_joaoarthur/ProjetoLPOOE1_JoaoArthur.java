1e84a114c0c21c935a01b3e9f86cff5a2dec9cd2
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2024-11-02 10:13:16 -0300

    Primeiro Commit e criação do projeto

---
+++ src/main/java/com/mycompany/projetolpooe1_joaoarthur/ProjetoLPOOE1_JoaoArthur.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetolpooe1_joaoarthur;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProjetoLPOOE1_JoaoArthur {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
