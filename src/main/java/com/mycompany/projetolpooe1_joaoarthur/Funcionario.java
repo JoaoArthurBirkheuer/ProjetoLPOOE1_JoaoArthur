95e1decc5d82a9fe58c5ad6c0df3bd5ef8f4a344
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2024-11-04 18:46:25 -0300

    Adição de atributo em Funcionário e mudanças nos testes de persistência e diagrama de classes

--- src/main/java/com/mycompany/projetolpooe1_joaoarthur/Funcionario.java
+++ src/main/java/com/mycompany/projetolpooe1_joaoarthur/Funcionario.java
@@ -1,16 +1,27 @@
 package com.mycompany.projetolpooe1_joaoarthur;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_funcionario")
 public class Funcionario extends Pessoa {
+    
+    @Column(nullable = false)
+    private String cargo;
     public Funcionario(String nome, String cpf, String email) {
         super(nome, cpf, email, true);
     }
-
     public Funcionario() {
         super();
+    }
+    
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
 }
--- src/main/java/com/mycompany/projetolpooe1_joaoarthur/ProjetoLPOOE1_JoaoArthur.java
+++ src/main/java/com/mycompany/projetolpooe1_joaoarthur/ProjetoLPOOE1_JoaoArthur.java
@@ -22,34 +22,36 @@ public class ProjetoLPOOE1_JoaoArthur {
             Usuario usuario2 = new Usuario("Nome 2", "98765432100", "usuario2@example.com");
             em.persist(usuario2);
 
-            // Adicionar Funcionários
+            // Adicionar Funcionários com o atributo cargo
             Funcionario funcionario1 = new Funcionario("Funcionario 1", "11122233300", "funcionario1@example.com");
+            funcionario1.setCargo("Bibliotecário");
             em.persist(funcionario1);
 
             Funcionario funcionario2 = new Funcionario("Funcionario 2", "44455566600", "funcionario2@example.com");
+            funcionario2.setCargo("Auxiliar");
             em.persist(funcionario2);
             
-            //Inserindo Funcionário aqui para testar id
-            Usuario usuario3 = new Usuario("Nome 3","02039627022","joao@gmail.com");
+            // Inserindo outro usuário para teste
+            Usuario usuario3 = new Usuario("Nome 3", "02039627022", "joao@gmail.com");
             em.persist(usuario3);
 
             // Adicionar Livros
             Livro livro1 = new Livro();
-            livro1.setTitulo("Java 1");
+            livro1.setTitulo("Java Basics");
             livro1.setAutor("Autor A");
             em.persist(livro1);
 
             Livro livro2 = new Livro();
-            livro2.setTitulo("Java 2");
+            livro2.setTitulo("Advanced Java");
             livro2.setAutor("Autor B");
             em.persist(livro2);
 
             Livro livro3 = new Livro();
-            livro3.setTitulo("Java 3");
+            livro3.setTitulo("Java Patterns");
             livro3.setAutor("Autor C");
             em.persist(livro3);
 
-            // Adicionar Empréstimos Exemplos
+            // Adicionar Empréstimos
             Emprestimo emprestimo1 = new Emprestimo();
             emprestimo1.setUsuario(usuario1);
             emprestimo1.setDataEmprestimo(LocalDate.now());
@@ -59,33 +61,10 @@ public class ProjetoLPOOE1_JoaoArthur {
 
             Emprestimo emprestimo2 = new Emprestimo();
             emprestimo2.setFuncionario(funcionario2);
-            // Campo nao permitido para ser null
             emprestimo2.setDataEmprestimo(LocalDate.now().minusDays(10));
-            // Campo permitido para ser null
-            // emprestimo2.setDataDevolucao(LocalDate.now().plusDays(4));
+            emprestimo2.setDataDevolucao(LocalDate.now().plusDays(4));
             emprestimo2.setLivrosEmprestados(Set.of(livro3));
             em.persist(emprestimo2);
-            
-            // Tenta encontrar usuario de código 6 (6L == 6 long)
-            // Para outras consultas, precisa de query personalizada
-            Usuario usuario6 = em.find(Usuario.class, 6L);
-            if (usuario6 == null) {
-            System.out.println("Usuário com ID 6 não existe.");
-            }  
-            else {
-            em.remove(usuario6);
-            System.out.println("Usuário com ID 6 removido com sucesso.");
-            }
-
-            // Outro teste
-            em.find(Usuario.class, 2L);
-            if (usuario2 == null) {
-            System.out.println("Usuário com ID 2 não existe.");
-            } 
-            else {
-            em.remove(usuario2);
-            System.out.println("Usuário com ID 2 removido com sucesso.");
-            }
             
             em.getTransaction().commit();
 
@@ -93,11 +72,10 @@ public class ProjetoLPOOE1_JoaoArthur {
 
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
-                System.out.println("Problema na transação, revertendo...\n\n\n");
+                System.out.println("Problema na transação, revertendo...");
                 em.getTransaction().rollback();
             }
-            // Para observar log de erros
-            // e.printStackTrace();
+            e.printStackTrace();
         } finally {
             em.close();
             emf.close();
